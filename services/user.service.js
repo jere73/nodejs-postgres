34ecaabddeaac3106b1084beebca2ce44061ad8c
Jeremias Osuna <jeremias.osuna@trocafone.com>
2022-09-18 23:25:47 -0300

    Pool connections applied.

--- services/user.service.js
+++ services/user.service.js
@@ -1,17 +1,20 @@
 const boom = require('@hapi/boom');
-const getConnection = require('../libs/postgres');
+const pool = require('../libs/postgres.pool');
 
 class UserService {
-  constructor() {}
+  constructor() {
+    this.pool = pool;
+    this.pool.on('error', (err) => console.err(err));
+  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    const client = await getConnection();
-    const rta = await client.query('SELECT * FROM tasks');
-    return rta.rows;
+    const query = 'SELECT * FROM tasks';
+    const response = await this.pool.query(query);
+    return response.rows;
   }
 
   async findOne(id) {
